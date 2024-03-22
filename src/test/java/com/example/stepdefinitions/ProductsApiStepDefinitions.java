6ac088ac6cb79e185cbb7ae3bf8bc2c695b36d18
Jeenu John Dennis <jeenujohnd@gmail.com>
2024-03-21 23:20:44 -0500

    Products API feature added -Get All Products List

--- src/test/java/com/example/stepdefinitions/ProductsApiStepDefinitions.java
+++ src/test/java/com/example/stepdefinitions/ProductsApiStepDefinitions.java
@@ -6,25 +6,41 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.annotations.Steps;
 
-import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import io.restassured.response.Response;
+
+import static net.serenitybdd.rest.SerenityRest.*;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ProductsApiStepDefinitions {
 
+    private Response response;
     @Steps
     ProductsApiSteps productsApiSteps;
 
-    @Given("the application is running")
-    public void the_application_is_running() {
-        productsApiSteps.isAppRunning();
+    @Given("the base URL \"([^\"]*)\"$")
+    public void the_base_url(String baseUrl) {
+        //productsApiSteps.isAppRunning();
+        rest().baseUri(baseUrl);
     }
 
-    @When("the user checks all products list")
-    public void the_user_checks_all_products_list() {
-        productsApiSteps.checkAllProductList();
+    @When("GET request to \"([^\"]*)\"$")
+    public void get_request_to(String endpoint) {
+        response = get(endpoint);
     }
 
-    @Then("the API should return complete details of all the products")
-    public void the_api_should_return_complete_details_of_all_the_products() {
-        restAssuredThat(response -> response.statusCode(200));
+    @Then("the API should return {int} status code")
+    public void the_api_should_return_status_code(int statusCode) {
+        response.then().statusCode(statusCode);
+    }
+
+    @Then("the size of the {string} element have more than {int} times")
+    public void the_size_of_the_element_have_more_than_times(String string, Integer int1) {
+        String jsonString= response.body().print();
+        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray productArray = jsonObject.getJSONArray(string);
+        assert(productArray.length() > int1);
     }
 }
--- src/test/resources/features/products/ProductsApi.feature
+++ src/test/resources/features/products/ProductsApi.feature
@@ -4,9 +4,10 @@
 Feature: Products API feature
 
   Scenario: Get All Products List
-      Given the application is running
-      When the user checks all products list
-      Then the API should return complete details of all the products
+      Given the base URL "https://automationexercise.com"
+      When GET request to "https://automationexercise.com/api/productsList"
+      Then the API should return 200 status code
+      And the size of the "products" element have more than 10 times
 
 
 
